b23b80b0970ba8a9e16a66a513fef857d3b65ade
Markosipo <elpaladindelajusticia@gmail.com>
2015-05-27 23:19:49 -0300

    Clase de prueba para el nuevo branch

---
+++ TestTrabajo/src/TestInterface.java
@@ -0,0 +1,12 @@
+
+public interface TestInterface {
+	/**
+	 * NOMBRE constant
+	 */
+	public static final String NOMBRE = "nombre";
+	
+	/**
+	 * ZERO constant
+	 */
+	public static final int ZERO = 0;
+}
